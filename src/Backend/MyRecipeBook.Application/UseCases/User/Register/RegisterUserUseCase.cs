efe1ba2dcf9429edfe269bc582c0458657d0f0e8
jvictorportela <jvictorportela30@outlook.com>
2025-01-08 08:34:16 -0300

    FluentValidation implant

---
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
@@ -0,0 +1,37 @@
+﻿using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+
+namespace MyRecipeBook.Application.UseCases.User.Register;
+
+public class RegisterUserUseCase
+{
+    public ResponseRegisteredUserJson Execute(RequestRegisterUserJson request)
+    {
+        //Validar a request
+        Validate(request);
+
+        //Mapear a request em uma entidade
+
+        //Criptografar a senha
+
+        //Salvar no banco de dados
+
+        return new ResponseRegisteredUserJson
+        {
+            Name = request.Name
+        };
+    }
+
+    private void Validate(RequestRegisterUserJson request)
+    {
+        var validator = new RegisterUserValidator();
+
+        var result = validator.Validate(request);
+
+        if (!result.IsValid)
+        {
+            var errorsMessages = result.Errors.Select(e => e.ErrorMessage);
+            throw new Exception();
+        }
+    }
+}
---
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using MyRecipeBook.Communication.Requests;
+
+namespace MyRecipeBook.Application.UseCases.User.Register;
+
+public class RegisterUserValidator : AbstractValidator<RequestRegisterUserJson>
+{
+    public RegisterUserValidator()
+    {
+        RuleFor(user => user.Name).NotEmpty();
+        RuleFor(user => user.Email).EmailAddress().NotEmpty();
+        RuleFor(user => user.Password.Length).GreaterThanOrEqualTo(6);
+    }
+}
