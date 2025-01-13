353197f264ec904cb3f6152f450c1373ea93734a
jvictorportela <jvictorportela30@outlook.com>
2025-01-13 09:39:45 -0300

    test dependencies injection

--- src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
@@ -1,22 +1,36 @@
-﻿using MyRecipeBook.Communication.Requests;
+﻿using MyRecipeBook.Application.Services.AutoMapper;
+using MyRecipeBook.Application.Services.Cryptography;
+using MyRecipeBook.Communication.Requests;
 using MyRecipeBook.Communication.Responses;
+using MyRecipeBook.Domain.Repositories.User;
 using MyRecipeBook.Exceptions.ExceptionsBase;
 
 namespace MyRecipeBook.Application.UseCases.User.Register;
 
 public class RegisterUserUseCase
 {
-    public ResponseRegisteredUserJson Execute(RequestRegisterUserJson request)
+    private readonly IUserWriteOnlyRepository _writeRepository;
+    private readonly IUserReadOnlyRepository _readRepository;
+
+    public async Task<ResponseRegisteredUserJson> Execute(RequestRegisterUserJson request)
     {
         //Validar a request
         Validate(request);
 
         //Mapear a request em uma entidade
-         
+        var autoMapper = new AutoMapper.MapperConfiguration(options =>
+        {
+            options.AddProfile(new AutoMapping());
+        }).CreateMapper();
+
+        var user = autoMapper.Map<Domain.Entities.User>(request);
 
         //Criptografar a senha
+        var passwordCriptography = new PasswordEncrypter();
+        user.Password = passwordCriptography.Encrypt(request.Password);
 
         //Salvar no banco de dados
+        await _writeRepository.Add(user);
 
         return new ResponseRegisteredUserJson
         {
---
+++ src/Backend/MyRecipeBook.Infrastructure/DataAccess/MyRecipeBookDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyRecipeBook.Infrastructure.DataAccess;
+
+public class MyRecipeBookDbContext : DbContext
+{
+    public MyRecipeBookDbContext(DbContextOptions options) : base(options) { }
+
+    public DbSet<Domain.Entities.User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(MyRecipeBookDbContext).Assembly);
+    }
+}
