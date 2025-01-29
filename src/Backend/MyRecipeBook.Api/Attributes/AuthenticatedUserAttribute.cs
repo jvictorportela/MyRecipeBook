dbde91a240b8633a313b603cc3ee8f5ec98c104e
jvictorportela <jvictorportela30@outlook.com>
2025-01-29 07:28:43 -0300

    class for validate token created

---
+++ src/Backend/MyRecipeBook.Api/Attributes/AuthenticatedUserAttribute.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.Api.Filters;
+
+namespace MyRecipeBook.Api.Attributes;
+
+public class AuthenticatedUserAttribute : TypeFilterAttribute
+{
+    public AuthenticatedUserAttribute() : base(typeof(AuthenticatedUserFilter))
+    {
+    }
+}
--- src/Shared/MyRecipeBook.Exceptions/ResourceMessagesExceptions.Designer.cs
+++ src/Shared/MyRecipeBook.Exceptions/ResourceMessagesExceptions.Designer.cs
@@ -106,6 +106,15 @@ namespace MyRecipeBook.Exceptions {
         }
         
         /// <summary>
+        ///   Consulta uma cadeia de caracteres localizada semelhante a Token is null!.
+        /// </summary>
+        public static string NO_TOKEN {
+            get {
+                return ResourceManager.GetString("NO_TOKEN", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Consulta uma cadeia de caracteres localizada semelhante a The password must contain 6 or more characters..
         /// </summary>
         public static string PASSWORD_LENGTH {
@@ -120,6 +129,15 @@ namespace MyRecipeBook.Exceptions {
         public static string UNKNOWN_ERROR {
             get {
                 return ResourceManager.GetString("UNKNOWN_ERROR", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta uma cadeia de caracteres localizada semelhante a The user canot permission..
+        /// </summary>
+        public static string USER_WITHOUT_PERMISSION_ACCESS_RESOURCE {
+            get {
+                return ResourceManager.GetString("USER_WITHOUT_PERMISSION_ACCESS_RESOURCE", resourceCulture);
             }
         }
     }
