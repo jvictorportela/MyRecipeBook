29c0a46f47e6e30b7c006b1d5e97e8902949862f
jvictorportela <jvictorportela30@outlook.com>
2026-07-04 14:44:19 -0300

    Endpoint for register user created, but not implemented.

---
+++ src/Backend/MyRecipeBook.API/Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.Application.UseCases.User.Register;
+using MyRecipeBook.Communication.Requests;
+
+namespace MyRecipeBook.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Register([FromBody]RequestRegisterUserAccountJson request)
+    {
+        var useCase = new RegisterUserAccountUseCase();
+
+        useCase.Execute(request);
+
+        return Created();
+    }
+}
